13439da9480fd1dce0760ad24025df76bd6b15dc
Miguel <migueldelalibera9@gmail.com>
2025-03-03 19:24:40 -0300

    Adiciona SecurityFilter que verifica o token JWT em cada request

--- src/main/java/com/example/authentication_api/security/SecurityConfig.java
+++ src/main/java/com/example/authentication_api/security/SecurityConfig.java
@@ -1,10 +1,10 @@
 package com.example.authentication_api.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,10 +12,14 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    SecurityFilter securityFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
@@ -28,7 +32,7 @@ public class SecurityConfig {
                                 .requestMatchers(HttpMethod.DELETE, "/car/**").hasRole("ADMIN")
                                 .requestMatchers("/car").authenticated()
                                 .anyRequest().permitAll())
-                .httpBasic(Customizer.withDefaults())
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
---
+++ src/main/java/com/example/authentication_api/security/SecurityFilter.java
@@ -0,0 +1,48 @@
+package com.example.authentication_api.security;
+
+import com.example.authentication_api.model.entity.User;
+import com.example.authentication_api.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = recoverToken(request);
+
+        if (token != null) {
+            String username = tokenService.validateToken(token);
+            User user = userRepository.findByEmail(username);
+
+            UsernamePasswordAuthenticationToken usernamePassword = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(usernamePassword);
+
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        if (header == null) {
+            return null;
+        }
+        return header.replace("Bearer ", "");
+    }
+}
