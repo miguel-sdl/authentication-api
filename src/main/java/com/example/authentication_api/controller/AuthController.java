072db41bb4c5d146c2f4eb3ee0c9183ca30c6d05
Miguel <migueldelalibera9@gmail.com>
2025-03-09 16:02:38 -0300

    Adiciona os status retornados de cada endpoint com swagger

--- src/main/java/com/example/authentication_api/controller/AuthController.java
+++ src/main/java/com/example/authentication_api/controller/AuthController.java
@@ -5,6 +5,8 @@ import com.example.authentication_api.model.dto.LoginResponseDTO;
 import com.example.authentication_api.model.dto.RegisterDTO;
 import com.example.authentication_api.model.entity.User;
 import com.example.authentication_api.service.AuthService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,6 +22,11 @@ public class AuthController {
     AuthService authService;
 
     @PostMapping("/register")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Quando o usuario e cadastrado."),
+            @ApiResponse(responseCode = "400", description = "Quando o usuario ja existe ou a validacao de campos falha.")
+    }
+    )
     public ResponseEntity<Void> register(@RequestBody @Valid RegisterDTO dto) {
         authService.register(dto);
 
@@ -27,12 +34,22 @@ public class AuthController {
     }
 
     @PostMapping("/login")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Quando retorna o token JWT."),
+            @ApiResponse(responseCode = "400", description = "Quando a validacao de campos falha."),
+            @ApiResponse(responseCode = "403", description = "Quando usuario e/ou senha estao incorretos.")
+
+    })
     public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid LoginDTO dto) {
         String token = authService.login(dto);
         return ResponseEntity.ok(new LoginResponseDTO(token));
     }
 
     @GetMapping
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Quando retorna todos usuarios"),
+            @ApiResponse(responseCode = "401", description = "Quando nao esta autenticado")
+    })
     public ResponseEntity<List<User>> findAll() {
         return ResponseEntity.ok(authService.findAll());
     }
--- src/main/java/com/example/authentication_api/controller/CarController.java
+++ src/main/java/com/example/authentication_api/controller/CarController.java
@@ -4,6 +4,8 @@ import com.example.authentication_api.model.dto.CarPostDTO;
 import com.example.authentication_api.model.dto.CarPutDTO;
 import com.example.authentication_api.model.entity.Car;
 import com.example.authentication_api.service.CarService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,32 +21,63 @@ public class CarController {
     private CarService carService;
 
     @GetMapping
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Quando retorna todos carros."),
+            @ApiResponse(responseCode = "401", description = "Quando nao esta autenticado.")
+    })
     public ResponseEntity<List<Car>> findAll() {
         return ResponseEntity.ok(carService.findAll());
     }
 
     @GetMapping("/find")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Quando encontra os carros pelo nome."),
+            @ApiResponse(responseCode = "401", description = "Quando nao esta autenticado.")
+    })
     public ResponseEntity<List<Car>> findByName(@RequestParam String name) {
         return ResponseEntity.ok(carService.findByName(name));
     }
 
     @GetMapping("{id}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Quando encontra o carro pelo  id."),
+            @ApiResponse(responseCode = "400", description = "Quando nao encontra o carro pelo id."),
+            @ApiResponse(responseCode = "401", description = "Quando nao esta autenticado.")
+    })
     public ResponseEntity<Car> findById(@PathVariable int id) {
         return ResponseEntity.ok(carService.findById(id));
     }
 
     @PostMapping
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Quando salva o carro."),
+            @ApiResponse(responseCode = "400", description = "Quando a validacao de campos falha."),
+            @ApiResponse(responseCode = "401", description = "Quando nao esta autenticado.")
+    })
     public ResponseEntity<Car> save(@RequestBody @Valid CarPostDTO dto) {
         return new ResponseEntity<>(carService.save(dto), HttpStatus.CREATED);
     }
 
     @PutMapping
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Quando atualiza o carro."),
+            @ApiResponse(responseCode = "400", description = "Quando nao encontra o carro para atualizar ou a validacao de campos falha."),
+            @ApiResponse(responseCode = "401", description = "Quando nao esta autenticado."),
+            @ApiResponse(responseCode = "403", description = "Quando esta autenticado, mas nao como admin.")
+    })
     public ResponseEntity<Void> update(@RequestBody @Valid CarPutDTO dto) {
         carService.update(dto);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping("/{id}")
+    @PutMapping
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Quando remove o carro."),
+            @ApiResponse(responseCode = "400", description = "Quando nao encontra o carro para remover."),
+            @ApiResponse(responseCode = "401", description = "Quando nao esta autenticado."),
+            @ApiResponse(responseCode = "403", description = "Quando esta autenticado, mas nao como admin.")
+    })
     public ResponseEntity<Void> delete(@PathVariable int id) {
         carService.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
